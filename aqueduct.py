af012f282f749f045788e8559e964a1a21069669
grambo88 <gosnopro@gmail.com>
2024-03-17 11:40:44 -0400

    initial data upload

---
+++ aqueduct.py
@@ -0,0 +1,186 @@
+from selenium import webdriver
+import time
+from time import sleep
+import requests
+import sys
+import math
+from datetime import date
+import  os 
+import datetime
+import time 
+import sched
+import webbrowser
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+class MyWebdriver:
+
+    def __init__(self, webdriverval):
+        driver = webdriverval
+
+
+    def remove_quotes(self):  # not in use   # possibly take in list, return string?
+        data = ""
+        with open("new_info.csv") as file:
+            data = file.read().replace('"', "'").replace('[', '').replace(']', '').replace("'", '').replace('.', '').replace(' ,', '')
+
+        with open("new_info_mod.csv", 'a') as file:
+            file.write(data)
+
+    def toBinary(a):
+          l,m=[],[]
+          # print("''Hello world'' in binary is ") 
+          # print(toBinary("Hello world"))
+          for i in a:
+            l.append(ord(i))
+          for i in l:
+            m.append(int(bin(i)[2:]))
+          return m
+
+
+    def table_scrape(self, race_date):
+        num_tables = driver.find_elements('tag name', 'table')
+        race_count = len(num_tables) / 3
+        tables = driver.find_elements('xpath', "//table[@class='table table-sm table-hrn table-entries']")
+        scratched = driver.find_elements('xpath', "//*[@class='scratched']")
+        col_headers = driver.find_elements('tag name', 'thead')
+        payouts = driver.find_elements('xpath', "//table[@class='table table-hrn table-payouts']")
+        race_info = driver.find_elements('xpath', "//*[@class='col-lg-auto flex-grow-1 race-distance']")
+        race_purse = driver.find_elements('xpath', "//*[@class='col-lg-auto race-purse']")
+
+        path = 'data/aqueduct-test/' + str(race_date.strip("\n")) + '/'
+
+        os.makedirs(path, exist_ok=True)
+
+        race = 0
+        # sleep(5)
+        for money, x in zip(race_purse, race_info):
+            race = race + 1
+            np = path + "race" + str(race) + "/"
+            os.makedirs(np, exist_ok=True)
+            price = money.text
+            price = price.replace('[', '').replace(']', '')
+            price = price.replace('Purse: ', '').replace(',', '')
+            price = price.replace('$', '')
+            rinfo = [x.text]
+            rinfo = [item.replace('[', '') for item in rinfo]
+            rinfo = [item.replace(']', '') for item in rinfo]
+            rinfo = [item.replace(',000', '000') for item in rinfo]
+            rinfo = [item.replace(',500', '500') for item in rinfo]
+            rinfo = [item.replace('$', '') for item in rinfo]
+            rinfo = [item.replace('-', '') for item in rinfo]
+            rinfo = [item.replace('Dirt', '0') for item in rinfo]
+            rinfo = [item.replace('Turf', '1') for item in rinfo]
+            race_date = race_date.replace('-', '')
+            rinfo.insert(0, race_date.replace('\n', '')) # adds date
+            rinfo.insert(1, str(race)) # adds race_num
+            rinfo.insert(2, price) # add purse
+            # if rinfo == "Dirt":
+            #     rinfo.replace("Dirt", int(0))
+            # if rinfo == "Turf":
+            #     rinfo.replace("Turf", int(1))
+            rinfo = str(rinfo) # date, race, purse, dist, dirt/turf, type
+            rinfo = rinfo.replace('"', "'").replace('[', '').replace(']', '').replace("'", '').replace('.', '').replace(' ,', '').replace('/', '')
+
+            new_path = np + 'new_info.csv'
+            sys.stdout = open(new_path, 'a')
+            print(rinfo)
+
+        # print('starting "for table in payouts: " ')
+        ra = 0
+        # sleep(5)
+        for table in payouts:
+            ra = ra + 1
+            #winner_stats = []
+            #new_stats = []
+            for row in table.find_elements('tag name', 'tr'):
+                winner_stats = []
+                new_stats = []
+                for cell in row.find_elements('tag name', 'td'):
+                    cell = cell.text
+                    winner_stats.append(cell)
+                if winner_stats == []:
+                    # print('winner_stats == []')
+                    pass
+                else:
+                    # print('payout checkpoint')
+                    winner_stats.pop(1)
+                    winner_stats_str = str(winner_stats)
+                    winner_stats_str = winner_stats_str.replace('"', "'").replace('[', '').replace(']', '').replace("'", '').replace(' ,', '')
+                    nppp = path + "race" + str(ra) + "/"
+                    new_path = nppp + 'payouts.csv'  
+                    sys.stdout = open(new_path, 'a')
+                    print(winner_stats_str)
+
+        # sleep(5)
+        n = 0
+        for table in tables:
+            n = n + 1
+            for entry in table.find_elements('tag name', 'tr'):
+                if entry not in scratched:
+                    horse_stats = []
+                    new_stat = []
+                    for stat in entry.find_elements('tag name', 'td'):
+                        stat = stat.text
+                        m = stat.replace("\n", "', '").replace(", Jr.", " Jr.")
+                        horse_stats.append(m)
+                    if horse_stats == []:
+                        pass
+                    else:
+                        npppp = path + "race" + str(n) + "/"
+                        new_path = npppp + 'entries.csv'
+                        horse_stats_str = str(horse_stats)
+                        horse_stats_str = horse_stats_str.replace('"', "'").replace('[', '').replace(']', '').replace("'", '').replace('.', '').replace(' ,', '')
+                        horse_stats_str = horse_stats_str[2:]
+                        sys.stdout = open(new_path, 'a')
+                        print(horse_stats_str)
+                else:
+                    pass
+
+
+
+if __name__ == '__main__':
+
+    start_time = time.time()
+    print("entered __main__")
+    
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument('--ignore-ssl-errors')
+    chrome_options.add_argument("--start-maximized")
+    # chrome_options.page_load_strategy = "eager"
+    # chrome_options.add_argument("--no-sandbox")
+    # chrome_options.page_load_strategy = 'none'
+    # chrome_options.add_argument("--headless")
+    print('got here')
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+file1 = open('2022-aqueduct-race-days.txt', 'r')
+
+for item in file1:
+    sleep(3)
+    str_item = str(item)
+    N = 11
+    length = len(str_item)
+    date = str_item[length - N:]
+    url = item.strip()
+    request = requests.head(url)
+    print("checking if site exists")
+    sleep(3)
+    if request.status_code == 200: # maybe from here, create function below containing next steps??
+        print("launching driver")
+        driver.get(url)
+        sleep(3)
+        driver.refresh()
+        obj = MyWebdriver(driver)
+        sleep(3)
+        obj.table_scrape(date)
+        sleep(3)
+
+    else:
+        pass
---
+++ test.py
@@ -0,0 +1,229 @@
+import os
+import csv
+import pandas as pd
+from time import sleep
+import numpy as np 
+
+filename = 'data.csv'
+filename1 = 'data1.csv'
+filename2 = 'data2.csv'
+filename3 = 'data3.csv'
+
+def read_csv_file(file_path):
+    data = []
+    with open(file_path, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            data.append(row)
+    return data
+
+def append_dataframes(new_info_df, entries_df, payouts_df):
+    # Create an empty list to store the appended dataframes
+    appended_dataframes = []
+
+    # Append dataframes to the list
+    appended_dataframes.append(new_info_df)
+    appended_dataframes.append(entries_df)
+    appended_dataframes.append(payouts_df)
+
+    return appended_dataframes
+
+
+def append_to_csv(df1, df2, df3, csv_filename):
+    # Concatenate the DataFrames into a list
+    concatenated_list = [df1, df2, df3]
+
+    # Convert the list to a Pandas DataFrame
+    concatenated_df = pd.concat(concatenated_list, axis=0, ignore_index=True)
+
+    # Append the DataFrame to the CSV file
+    concatenated_df.to_csv(csv_filename, mode='a', index=False, header=not pd.read_csv(csv_filename).shape[0])
+
+
+def predict_output(matrix1, matrix2):
+    # Ensure the input matrices have the correct shapes
+    assert matrix1.shape == (6,) , "Matrix1 should be of shape (1, 6)"
+    assert matrix2.shape == (16,6), "Matrix2 should be of shape (16, 6)"
+
+    # Perform some operation on the input matrices (you need to define the operation)
+    # For example, let's concatenate the matrices and reshape to get a 4x4 matrix
+    concatenated_matrix = np.concatenate((matrix1, matrix2), axis=0)
+    predicted_matrix = np.reshape(concatenated_matrix[:4, :], (4, 4))
+
+    return predicted_matrix
+
+
+def process_files(folder_path):
+    # List all files in the specified folder
+    aqueduct_path = os.listdir(folder_path)
+
+    filename = 'data.csv'
+    filename1 = 'data1.csv'
+    filename2 = 'data2.csv'
+    filename3 = 'data3.csv'
+
+
+    # Loop through each file in the folder
+    for date_dirs in aqueduct_path:
+        # Construct the full path to the file
+        date_path = os.path.join(str(folder_path), str(date_dirs))
+        # print(date_path)
+        race_list = os.listdir(date_path)
+        # print(race_list)
+        for race_dirs in race_list:
+            full_path = os.path.join(str(date_path), str(race_dirs))
+            #print(full_path)
+
+            test = os.listdir(full_path)
+            #print(test)
+
+            entries = full_path + '\\' + 'entries.csv'
+            new_info = full_path + '\\' + 'new_info.csv'
+            payouts = full_path + '\\' + 'payouts.csv'
+
+            # entries_df = pd.read_csv(entries, sep=',', header=None)
+            # new_info_df = pd.read_csv(new_info, sep=',', header=None)
+            # payouts_df = pd.read_csv(payouts, sep=',', header=None)
+
+
+            #print(entries_df.values)
+            #    print(entries_df, new_info_df, payouts_df)
+            # Trying to add '0,0,0,0,0,0' to the entries files that do not have 16 rows
+
+            file_paths = [entries, new_info, payouts]
+
+            # Initialize an empty list to store datasets
+            datasets = []
+            mod_dataset = []
+
+            #for file_path in file_paths:
+
+
+            npa_new_info = np.genfromtxt(new_info, delimiter=',')
+            npa_entries = np.genfromtxt(entries, delimiter=',')
+            npa_payouts = np.genfromtxt(payouts, delimiter=',')
+
+
+            mod_dataset.append(npa_new_info)
+            mod_dataset.append(npa_entries)
+            mod_dataset.append(npa_payouts)
+
+            print(npa_new_info.shape)
+            print(npa_entries.shape)
+
+            print('\n')
+            print(npa_new_info)
+            print('\n')
+            print(npa_entries)
+            print('\n')
+
+            # predict_output(npa_new_info, npa_entries)
+
+
+            print(mod_dataset)
+            print('\n')
+            print('success')
+            sleep(5)
+
+            # Loop through each file path and read the CSV into a DataFrame, then append it to the list
+            for file_path in file_paths:
+                df = pd.read_csv(file_path, header=None)
+                datasets.append(df)
+            print(datasets)
+            print('\n')
+            print('success')
+            sleep(5)
+
+
+            # pass
+
+            # datasets_array = [df.values for df in datasets]
+            # print(datasets_array)
+            # sleep(2)
+
+
+            # with open(new_info, 'r') as info:
+            #     info_reader = csv.reader(info)
+            #     info_data = list(info_reader)
+                
+            #     try:
+            #         with open(entries, 'r') as entrie:
+
+            #         info_data_array = np.array(info_data)
+            #         # print('here')
+            #         print(info_data_array)
+            #         sleep(2)
+            #     except:
+            #         pass
+            # Testing below:
+
+
+            # result_matrix = predict_output(new_info_df, entries_df)
+            # print("Input Matrix 1: ")
+            # print(new_info_df)
+            # print("Input Matrix 2: ")
+            # printt(entries_df)
+            # print('Predicted Output: ')
+            # print(result_matrix)
+
+
+            # csv_file_paths = [entries, new_info, payouts]
+            # data_list = [entries_df.to_string(index=False), new_info_df.to_string(index=False), payouts_df.to_string(index=False)]
+
+            # if len(entries_df) == 14:
+            #     print('data_list is equal to 14 not 16')
+            #     entries.write('0, 0, 0, 0, 0, 0')
+            #     print(new_info)
+            #     print(entries)
+                # entries_df.append('0, 0, 0, 0, 0, 0')
+                # entries_df.append('0, 0, 0, 0, 0, 0')
+
+                
+
+
+            data_list = [new_info_df, entries_df, payouts_df]
+
+            # print(type(data_list))
+            # print(len(data_list))
+            # data_list_df = pd.DataFrame(data_list)
+            # data_list.to_csv(filename3, sep=',', mode='a', index=False, encoding='utf-8')
+
+            # data_list.to_csv(filename3, sep=',', mode='a', index=False, encoding='utf-8')
+
+            # append_to_csv(new_info_df, entries_df, payouts_df, filename3)
+
+            
+            # print(data_list)
+            # print(len(data_list[0]), len(data_list[1]), len(data_list[2]))
+
+            # print(df.to_string(index=False))
+            # sleep(2)
+
+
+
+            # print(new_info_df)
+            # new_info_df.to_csv(filename, sep=',', mode='a', index=False, encoding='utf-8')
+            # #print(' ')
+            # print(entries_df)
+            # entries_df.to_csv(filename1, sep=',', mode='a', index=False, encoding='utf-8')
+            # #print(' ')
+            # print(payouts_df)
+            # payouts_df.to_csv(filename2, sep=',', mode='a', index=False, encoding='utf-8')
+            # print(' ')
+
+            pass
+
+
+
+# Specify the folder path you want to process
+# folder_path = "/path/to/your/folder"
+folder_path = "C:\\Users\\Tyler\\projects\\cybertooth-mod\\data\\aqueduct"
+folder_path_test = "C:\\Users\\Tyler\\projects\\cybertooth-mod\\data\\aqueduct1"
+folder_path_test1 = "C:\\Users\\Tyler\\projects\\cybertooth-mod\\data\\aqueduct-test"
+
+# Call the function to process files in the folder
+# process_files(folder_path)
+
+process_files(folder_path_test1)
+
+
